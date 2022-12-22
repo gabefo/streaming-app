ad5c0710f7f531201c081e7ef92ddd9867a1cb03
gabefo <gabrielfomiranda@gmail.com>
2022-12-22 04:23:34 -0300

    create pagination

--- pages/api/movies.ts
+++ pages/api/movies.ts
@@ -8,13 +8,15 @@ type ResponseError = {
 
 const handler = (req: NextApiRequest, res: NextApiResponse<Movie[] | ResponseError>) => {
   const { query } = req
-  const genre = query.genre as string
+  const category = query.category as string
 
-  if (!(genre in movies)) {
+  if (!(category in movies)) {
     return res.status(404).json({ message: 'Not found' } as ResponseError)
   }
 
-  const data = movies[genre]
+  const offset = query.offset ? parseInt(query.offset as string) : 0
+  const limit = query.limit ? parseInt(query.limit as string) : 24
+  const data = movies[category].slice(offset, offset + limit)
 
   res.status(200).json(data)
 }
