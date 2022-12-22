dcd7f142366544dee997f8e16b6fe677c563b47e
gabefo <gabrielfomiranda@gmail.com>
2022-12-22 04:23:20 -0300

    use .map to render sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import MovieSection from 'components/MovieSection'
+import Subheader from 'components/Subheader'
 import TitleAndMetaTags from 'components/TitleAndMetaTags'
 import MainLayout from 'layouts/MainLayout'
+import categories from 'lib/categories'
 import { ReactElement } from 'react'
 import { NextPageWithLayout } from './_app'
 
@@ -8,18 +10,10 @@ const Home: NextPageWithLayout = () => {
   return (
     <>
       <TitleAndMetaTags />
-      <MovieSection title="Popular movies" genre="popular" />
-      <MovieSection title="New Releases" genre="newRelease" />
-      <MovieSection title="Adventure movies" genre="adventure" />
-      <MovieSection title="Oscar-winning movies" genre="oscarWinning" />
-      <MovieSection title="Drama movies" genre="drama" />
-      <MovieSection title="Action movies" genre="action" />
-      <MovieSection title="Fantasy movies" genre="fantasy" />
-      <MovieSection title="Thriller movies" genre="thriller" />
-      <MovieSection title="Mystery movies" genre="mystery" />
-      <MovieSection title="Horror movies" genre="horror" />
-      <MovieSection title="Comedy movies" genre="comedy" />
-      <MovieSection title="Superhero movies" genre="superhero" />
+      <Subheader />
+      {categories.map((category) => (
+        <MovieSection key={category.id} category={category} />
+      ))}
     </>
   )
 }
