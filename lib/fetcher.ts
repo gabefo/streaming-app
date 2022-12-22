86261c021ec0a695696a97cd83a47a02a898bbe2
gabefo <gabrielfomiranda@gmail.com>
2022-12-22 04:21:45 -0300

    create fetcher

---
+++ lib/fetcher.ts
@@ -0,0 +1,10 @@
+export default async function fetcher(url: string) {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  if (res.status !== 200) {
+    throw new Error(data.message)
+  }
+
+  return data
+}
