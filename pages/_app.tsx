4c6ba953740bd52d48bd43074e7847ca9598cbba
gabefo <gabrielfomiranda@gmail.com>
2023-01-02 23:44:57 -0300

    add locales

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,61 @@
+import { addCollection } from '@iconify/react'
 import { TooltipProvider } from '@radix-ui/react-tooltip'
-import { NextPage } from 'next'
+import ProgressBar from 'components/ProgressBar'
+import type { NextPage } from 'next'
+import { appWithTranslation } from 'next-i18next'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { ReactElement, ReactNode } from 'react'
+import type { ReactElement, ReactNode } from 'react'
 import { darkTheme, globalCss } from 'stitches.config'
+
+// https://api.iconify.design/mdi.js?icons=arrow-left,arrow-right,brightness-4,brightness-7,chevron-left,chevron-right,close,github,search,star&pretty=1&callback=addCollection
+addCollection({
+  prefix: 'mdi',
+  icons: {
+    'arrow-left': {
+      body: '<path fill="currentColor" d="M20 11v2H8l5.5 5.5l-1.42 1.42L4.16 12l7.92-7.92L13.5 5.5L8 11h12Z"/>',
+    },
+    'arrow-right': {
+      body: '<path fill="currentColor" d="M4 11v2h12l-5.5 5.5l1.42 1.42L19.84 12l-7.92-7.92L10.5 5.5L16 11H4Z"/>',
+    },
+    'brightness-4': {
+      body: '<path fill="currentColor" d="M12 18c-.89 0-1.74-.2-2.5-.55C11.56 16.5 13 14.42 13 12c0-2.42-1.44-4.5-3.5-5.45C10.26 6.2 11.11 6 12 6a6 6 0 0 1 6 6a6 6 0 0 1-6 6m8-9.31V4h-4.69L12 .69L8.69 4H4v4.69L.69 12L4 15.31V20h4.69L12 23.31L15.31 20H20v-4.69L23.31 12L20 8.69Z"/>',
+    },
+    'brightness-7': {
+      body: '<path fill="currentColor" d="M12 8a4 4 0 0 0-4 4a4 4 0 0 0 4 4a4 4 0 0 0 4-4a4 4 0 0 0-4-4m0 10a6 6 0 0 1-6-6a6 6 0 0 1 6-6a6 6 0 0 1 6 6a6 6 0 0 1-6 6m8-9.31V4h-4.69L12 .69L8.69 4H4v4.69L.69 12L4 15.31V20h4.69L12 23.31L15.31 20H20v-4.69L23.31 12L20 8.69Z"/>',
+    },
+    'chevron-left': {
+      body: '<path fill="currentColor" d="M15.41 16.58L10.83 12l4.58-4.59L14 6l-6 6l6 6l1.41-1.42Z"/>',
+    },
+    'chevron-right': {
+      body: '<path fill="currentColor" d="M8.59 16.58L13.17 12L8.59 7.41L10 6l6 6l-6 6l-1.41-1.42Z"/>',
+    },
+    close: {
+      body: '<path fill="currentColor" d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"/>',
+    },
+    github: {
+      body: '<path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"/>',
+    },
+    search: {
+      body: '<path fill="currentColor" d="M9.5 3A6.5 6.5 0 0 1 16 9.5c0 1.61-.59 3.09-1.56 4.23l.27.27h.79l5 5l-1.5 1.5l-5-5v-.79l-.27-.27A6.516 6.516 0 0 1 9.5 16A6.5 6.5 0 0 1 3 9.5A6.5 6.5 0 0 1 9.5 3m0 2C7 5 5 7 5 9.5S7 14 9.5 14S14 12 14 9.5S12 5 9.5 5Z"/>',
+    },
+    star: {
+      body: '<path fill="currentColor" d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.62L12 2L9.19 8.62L2 9.24l5.45 4.73L5.82 21L12 17.27Z"/>',
+    },
+  },
+  width: 24,
+  height: 24,
+})
 
 const globalStyles = globalCss({
   ':root': {
-    $gap: '24px',
+    $gutterX: '24px',
     $header: '56px',
 
     '@md': {
       $header: '64px',
-      $gap: '56px',
+      $gutterX: '56px',
     },
   },
 
@@ -33,6 +75,14 @@ const globalStyles = globalCss({
     fontFamily: '$openSans',
     fontSize: '1rem',
     lineHeight: '1.5rem',
+  },
+
+  b: {
+    fontWeight: 600,
+  },
+
+  img: {
+    verticalAlign: 'baseline',
   },
 
   svg: {
@@ -55,7 +105,7 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   globalStyles()
 
   const getLayout = Component.getLayout ?? ((page) => page)
@@ -73,8 +123,11 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
       >
         <TooltipProvider delayDuration={100} disableHoverableContent>
           {getLayout(<Component {...pageProps} />)}
+          <ProgressBar />
         </TooltipProvider>
       </ThemeProvider>
     </>
   )
 }
+
+export default appWithTranslation(MyApp)
