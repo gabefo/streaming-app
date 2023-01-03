c6f8698b5aa29b1915f105da9f9fda68ab31ddf4
gabefo <gabrielfomiranda@gmail.com>
2023-01-02 23:46:17 -0300

    fetch TMDB API

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,39 @@ import MovieSection from 'components/MovieSection'
 import Subheader from 'components/Subheader'
 import TitleAndMetaTags from 'components/TitleAndMetaTags'
 import MainLayout from 'layouts/MainLayout'
-import categories from 'lib/categories'
-import { ReactElement } from 'react'
-import { NextPageWithLayout } from './_app'
+import { getGenreMovieList } from 'lib/tmdb/api'
+import type { Genre } from 'lib/tmdb/types'
+import type { GetStaticProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import type { ReactElement } from 'react'
+import type { NextPageWithLayout } from './_app'
 
-const Home: NextPageWithLayout = () => {
+type Props = {
+  genres: Genre[]
+}
+
+const Page: NextPageWithLayout<Props> = ({ genres }) => {
   return (
     <>
       <TitleAndMetaTags />
       <Subheader />
-      {categories.map((category) => (
-        <MovieSection key={category.id} category={category} />
+      {genres.map((genre) => (
+        <MovieSection key={genre.id} genre={genre} />
       ))}
     </>
   )
 }
 
-Home.getLayout = (page: ReactElement) => {
+Page.getLayout = (page: ReactElement) => {
   return <MainLayout>{page}</MainLayout>
 }
 
-export default Home
+export default Page
+
+export const getStaticProps: GetStaticProps<Props> = async ({ locale }) => {
+  const genres = await getGenreMovieList(locale)
+
+  return {
+    props: { ...(await serverSideTranslations(locale ?? 'en', ['common'])), genres },
+  }
+}
