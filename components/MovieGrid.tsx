c166f259e2b0681b56b01d225274c0c295c563c0
gabefo <gabrielfomiranda@gmail.com>
2023-01-02 23:30:44 -0300

    create MovieGrid

---
+++ components/MovieGrid.tsx
@@ -0,0 +1,20 @@
+import type { Movie } from 'lib/tmdb/types'
+import Grid from './Grid'
+import MovieCard from './MovieCard'
+import MovieSkeleton from './MovieSkeleton'
+
+type MovieListProps = {
+  movies?: Movie[]
+  loading?: boolean
+}
+
+export default function MovieGrid({ movies, loading }: MovieListProps) {
+  return (
+    <Grid columns={{ '@initial': 1, '@md': 2 }} css={{ mx: -8, rowGap: 8 }}>
+      {movies?.slice(0, 10).map((movie) => (
+        <MovieCard key={movie.id} movie={movie} direction="row" />
+      ))}
+      {loading ? Array.from(Array(20)).map((_, index) => <MovieSkeleton key={index} />) : null}
+    </Grid>
+  )
+}
