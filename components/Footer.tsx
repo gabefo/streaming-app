ee3a8123f53a46da00a6ba1d9d2c093ea52112a8
gabefo <gabrielfomiranda@gmail.com>
2023-01-02 23:27:05 -0300

    give TMDB attribution

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,25 +1,42 @@
 import { Icon } from '@iconify/react'
 import { styled } from 'stitches.config'
+import Container from './Container'
+import Flex from './Flex'
 import SocialLink from './SocialLink'
 import Text from './Text'
+import TMDBLogo from './TMDBLogo'
 
 const StyledFooter = styled('footer', {
   display: 'flex',
   alignItems: 'center',
-  justifyContent: 'space-between',
-  height: '$footer',
-  px: '$gap',
+  pb: 24,
+})
+
+const Separator = styled('div', {
+  my: 32,
+  borderBottom: 'thin solid $border',
 })
 
 export default function Footer() {
   return (
     <StyledFooter>
-      <Text variant="caption" color="secondary">
-        A project by Gabriel
-      </Text>
-      <SocialLink href="https://github.com/gabefo/streaming-app" target="_blank">
-        <Icon icon="mdi:github" />
-      </SocialLink>
+      <Container gutters>
+        <Separator />
+        <Flex direction="column" css={{ rowGap: 24 }}>
+          <TMDBLogo />
+          <Text variant="caption" color="secondary">
+            This product uses the TMDB API but is not endorsed or certified by TMDB.
+          </Text>
+        </Flex>
+        <Flex css={{ mt: 32, gap: 16 }}>
+          <SocialLink href="https://www.linkedin.com/in/gabrielfomiranda/" target="_blank">
+            <Icon icon="mdi:linkedin" />
+          </SocialLink>
+          <SocialLink href="https://github.com/gabefo/streaming-app" target="_blank">
+            <Icon icon="mdi:github" />
+          </SocialLink>
+        </Flex>
+      </Container>
     </StyledFooter>
   )
 }
