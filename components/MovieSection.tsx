278397ecc664f1a9e43f848fe084851891609e56
gabefo <gabrielfomiranda@gmail.com>
2023-01-02 23:33:34 -0300

    fetch the TMDB API

--- components/MovieSection.tsx
+++ components/MovieSection.tsx
@@ -1,9 +1,9 @@
 import { Icon } from '@iconify/react'
 import useInView from 'hooks/useInView'
-import Category from 'interfaces/category'
-import Movie from 'interfaces/movie'
 import fetcher from 'lib/fetcher'
+import type { Genre, MovieList } from 'lib/tmdb/types'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { useRef } from 'react'
 import { styled } from 'stitches.config'
 import useSWRImmutable from 'swr/immutable'
@@ -19,28 +19,34 @@ const Section = styled('section', {
   mt: 24,
 })
 
-interface SectionProps {
-  category: Category
+type SectionProps = {
+  genre: Genre
 }
 
-export default function MovieSection({ category }: SectionProps) {
+export default function MovieSection({ genre }: SectionProps) {
+  const { locale } = useRouter()
+
   const ref = useRef<HTMLDivElement>(null)
 
   const isInView = useInView(ref, { rootMargin: '-48px', once: true })
 
-  const { data } = useSWRImmutable<Movie[], Error>(
-    isInView ? `/api/movies?category=${category.id}` : null,
+  const { data } = useSWRImmutable<MovieList, Error>(
+    isInView
+      ? `/api/tmdb/discover/movie?&language=${locale}&sort_by=popularity.desc&page=1&with_genres=${genre.id}`
+      : null,
     fetcher
   )
+
+  const movies = data?.results
 
   return (
     <Section ref={ref}>
       <Container gutters css={{ mb: 8 }}>
         <Flex align="center" justify="between">
           <Text as="h6" variant="title">
-            {category.name}
+            {genre.name}
           </Text>
-          <Link href={category.href} passHref legacyBehavior>
+          <Link href={`/movies/genre/${genre.id}`} passHref legacyBehavior>
             <IconButton as="a" edge="end">
               <Icon icon="mdi:arrow-right" />
             </IconButton>
@@ -49,7 +55,7 @@ export default function MovieSection({ category }: SectionProps) {
       </Container>
       <Container>
         <ScrollView>
-          {(data || Array.from(Array<undefined>(24))).map((movie, index) => (
+          {(movies || Array.from(Array<undefined>(24))).map((movie, index) => (
             <View key={index}>{movie ? <MovieCard movie={movie} /> : <MovieSkeleton />}</View>
           ))}
         </ScrollView>
