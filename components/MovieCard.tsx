22fe9dd5a238840f26780a8ab6378183c2d4b9fb
gabefo <gabrielfomiranda@gmail.com>
2023-01-02 23:30:35 -0300

    add release year and rating

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,32 +1,23 @@
-import Movie from 'interfaces/movie'
+import { Icon } from '@iconify/react'
+import type { Movie } from 'lib/tmdb/types'
+import buildImageURL from 'lib/tmdb/buildImageURL'
 import Image from 'next/image'
+import Link from 'next/link'
 import { styled } from 'stitches.config'
-
-const Card = styled('div', {
-  position: 'relative',
-  display: 'flex',
-  flexDirection: 'column',
-  p: 8,
-  borderRadius: 8,
-  overflow: 'hidden',
-  cursor: 'pointer',
-
-  '&:hover': {
-    bg: '$hover',
-  },
-})
+import type { CSS, VariantProps } from 'stitches.config'
+import Text from './Text'
 
 const ImageContainer = styled('div', {
   position: 'relative',
-  pt: '133.3333%',
+  flexShrink: 0,
+  aspectRatio: '3 / 4',
   borderRadius: 8,
   boxShadow: '0 2px 4px 0 rgb(0, 0, 0, 0.2)',
   overflow: 'hidden',
-})
 
-const Content = styled('div', {
-  mt: 8,
-  minHeight: 40,
+  '& > img': {
+    objectFit: 'cover',
+  },
 })
 
 const Title = styled('div', {
@@ -40,28 +31,103 @@ const Title = styled('div', {
   textOverflow: 'ellipsis',
 })
 
-interface MovieCardProps {
+const Description = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  mt: 4,
+  rowGap: 4,
+  columnGap: 12,
+})
+
+const Rating = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: 4,
+  color: '$textSecondary',
+  fontSize: '0.875rem',
+  lineHeight: '1.25rem',
+})
+
+const Card = styled('a', {
+  position: 'relative',
+  display: 'flex',
+  height: '100%',
+  p: 8,
+  rowGap: 8,
+  columnGap: 16,
+  borderRadius: 8,
+  color: 'inherit',
+  textDecoration: 'none',
+  overflow: 'hidden',
+  cursor: 'pointer',
+
+  '&:hover': {
+    bg: '$hover',
+  },
+
+  variants: {
+    direction: {
+      row: {
+        flexDirection: 'row',
+
+        [`& ${ImageContainer}`]: {
+          width: 72,
+
+          '@sm': {
+            width: 96,
+          },
+        },
+
+        [`& ${Description}`]: {
+          flexDirection: 'column-reverse',
+          alignItems: 'flex-start',
+        },
+      },
+      column: {
+        flexDirection: 'column',
+      },
+    },
+  },
+
+  defaultVariants: {
+    direction: 'column',
+  },
+})
+
+type MovieCardProps = VariantProps<typeof Card> & {
+  css?: CSS
   movie: Movie
 }
 
-export default function MovieCard({ movie }: MovieCardProps) {
-  const { title, image } = movie
+export default function MovieCard({ movie, ...props }: MovieCardProps) {
+  const { id, title, poster_path, vote_average, release_date } = movie
 
   return (
-    <Card>
-      <ImageContainer>
-        {image ? (
-          <Image
-            src={image}
-            alt={title}
-            fill
-            sizes="(min-width: 1280px) 122px, (min-width: 960px) 16vw, (min-width: 600px) 25vw, 33vw"
-          />
-        ) : null}
-      </ImageContainer>
-      <Content>
-        <Title>{title}</Title>
-      </Content>
-    </Card>
+    <Link href={`/movies/${id}`} passHref legacyBehavior>
+      <Card {...props}>
+        <ImageContainer>
+          {poster_path ? (
+            <Image
+              src={buildImageURL(poster_path)}
+              alt={title}
+              fill
+              sizes="(min-width: 1280px) 122px, (min-width: 960px) 16vw, (min-width: 600px) 25vw, 33vw"
+            />
+          ) : null}
+        </ImageContainer>
+        <div>
+          <Title>{title}</Title>
+          <Description>
+            <Rating>
+              {(vote_average / 2).toFixed(1)}
+              <Icon icon="mdi:star" fontSize={12} />
+            </Rating>
+            <Text variant="caption" color="secondary">
+              {release_date.slice(0, 4)}
+            </Text>
+          </Description>
+        </div>
+      </Card>
+    </Link>
   )
 }
