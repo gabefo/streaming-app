80c943678467d5ef1c8733814b7089eb8d965d78
gabefo <gabrielfomiranda@gmail.com>
2023-01-04 16:46:52 -0300

    update layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,13 @@
 import { Icon } from '@iconify/react'
+import Link from 'next/link'
 import { styled } from 'stitches.config'
 import Container from './Container'
 import Flex from './Flex'
+import LanguageSelect from './LanguageSelect'
+import Logo from './Logo'
 import SocialLink from './SocialLink'
 import Text from './Text'
+import ThemeSelect from './ThemeSelect'
 import TMDBLogo from './TMDBLogo'
 
 const StyledFooter = styled('footer', {
@@ -17,25 +21,62 @@ const Separator = styled('div', {
   borderBottom: 'thin solid $border',
 })
 
+const Section = styled('section', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  mt: 32,
+  gap: 24,
+
+  '@md': {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+})
+
+const StyledLink = styled(Link, {
+  color: '$textSecondary',
+  fontSize: '0.875rem',
+  lineHeight: '2rem',
+  textDecoration: 'none',
+
+  '&:hover': {
+    color: '$text',
+  },
+})
+
 export default function Footer() {
   return (
     <StyledFooter>
       <Container gutters>
         <Separator />
-        <Flex direction="column" css={{ rowGap: 24 }}>
-          <TMDBLogo />
-          <Text variant="caption" color="secondary">
+        <Section>
+          <Logo />
+          <Flex direction="column">
+            <StyledLink href="/">Home</StyledLink>
+            <StyledLink href="/about">About</StyledLink>
+            <StyledLink href="/terms-of-service">Terms of Service</StyledLink>
+            <StyledLink href="/privacy">Privacy</StyledLink>
+          </Flex>
+          <Flex css={{ gap: 16 }}>
+            <SocialLink href="https://github.com/gabefo/streaming-app" target="_blank">
+              <Icon icon="mdi:github" />
+            </SocialLink>
+            <SocialLink href="https://www.linkedin.com/in/gabrielfomiranda/" target="_blank">
+              <Icon icon="mdi:linkedin" />
+            </SocialLink>
+          </Flex>
+          <Flex css={{ gap: 8 }}>
+            <LanguageSelect />
+            <ThemeSelect />
+          </Flex>
+        </Section>
+        <Section>
+          <Text variant="caption" color="disabled">
             This product uses the TMDB API but is not endorsed or certified by TMDB.
           </Text>
-        </Flex>
-        <Flex css={{ mt: 32, gap: 16 }}>
-          <SocialLink href="https://www.linkedin.com/in/gabrielfomiranda/" target="_blank">
-            <Icon icon="mdi:linkedin" />
-          </SocialLink>
-          <SocialLink href="https://github.com/gabefo/streaming-app" target="_blank">
-            <Icon icon="mdi:github" />
-          </SocialLink>
-        </Flex>
+          <TMDBLogo />
+        </Section>
       </Container>
     </StyledFooter>
   )
