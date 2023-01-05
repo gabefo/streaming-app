e96426aea00ea7c59b55ae9049c6a94638a57724
gabefo <gabrielfomiranda@gmail.com>
2023-01-04 23:37:36 -0300

    fixed width

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -42,7 +42,14 @@ export default function Footer() {
           justify="between"
           css={{ gap: 24 }}
         >
-          <Flex direction="column" css={{ maxWidth: 240, gap: 24 }}>
+          <Flex
+            direction="column"
+            align="start"
+            css={{
+              gap: 24,
+              '@md': { maxWidth: 240 },
+            }}
+          >
             <TMDBLogo />
             <Text variant="caption" color="disabled">
               This product uses the TMDB API but is not endorsed or certified by TMDB.
@@ -59,13 +66,13 @@ export default function Footer() {
               </SocialLink>
             </Flex>
           </Flex>
-          <Flex direction="column" css={{ gap: 12 }}>
+          <Flex direction="column" css={{ minWidth: 120, gap: 12 }}>
             <Text variant="overline">Watch</Text>
             <StyledLink href="/">Home</StyledLink>
             <StyledLink href="/about">About</StyledLink>
             <StyledLink href="/contact">Contact</StyledLink>
           </Flex>
-          <Flex direction="column" css={{ gap: 12 }}>
+          <Flex direction="column" css={{ minWidth: 120, gap: 12 }}>
             <Text variant="overline">Legal</Text>
             <StyledLink href="/terms-of-service">Terms of Service</StyledLink>
             <StyledLink href="/privacy">Privacy</StyledLink>
