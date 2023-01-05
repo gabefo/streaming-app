0d9d0cb1352982fc1a5e56906247246e4b8d3b79
gabefo <gabrielfomiranda@gmail.com>
2023-01-04 23:05:36 -0300

    add padding and border-radius

--- components/SocialLink.tsx
+++ components/SocialLink.tsx
@@ -1,6 +1,8 @@
 import { styled } from 'stitches.config'
 
 const SocialLink = styled('a', {
+  p: 8,
+  borderRadius: '50%',
   color: '$textSecondary',
   fontSize: 24,
   lineHeight: 1,
@@ -8,6 +10,7 @@ const SocialLink = styled('a', {
 
   '&:hover': {
     color: '$text',
+    bg: '$hover',
   },
 })
 
