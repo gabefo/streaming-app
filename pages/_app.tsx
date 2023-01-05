308343a01e64bb894ce9bb0574998753172ce5de
gabefo <gabrielfomiranda@gmail.com>
2023-01-05 16:21:48 -0300

    update icons bundle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ import Head from 'next/head'
 import type { ReactElement, ReactNode } from 'react'
 import { darkTheme, globalCss } from 'stitches.config'
 
-// https://api.iconify.design/mdi.js?icons=arrow-left,arrow-right,check,chevron-left,chevron-right,close,github,linkedin,menu-down,menu-up,search,star,web&pretty=1&callback=addCollection
+// https://api.iconify.design/mdi.js?icons=arrow-left,arrow-right,check,chevron-left,chevron-right,close,github,linkedin,menu-down,menu-up,play-outline,search,star,web&pretty=1&callback=addCollection
 addCollection({
   prefix: 'mdi',
   icons: {
@@ -42,6 +42,9 @@ addCollection({
     },
     'menu-up': {
       body: '<path fill="currentColor" d="m7 15l5-5l5 5H7Z"/>',
+    },
+    'play-outline': {
+      body: '<path fill="currentColor" d="M8.5 8.64L13.77 12L8.5 15.36V8.64M6.5 5v14l11-7"/>',
     },
     search: {
       body: '<path fill="currentColor" d="M9.5 3A6.5 6.5 0 0 1 16 9.5c0 1.61-.59 3.09-1.56 4.23l.27.27h.79l5 5l-1.5 1.5l-5-5v-.79l-.27-.27A6.516 6.516 0 0 1 9.5 16A6.5 6.5 0 0 1 3 9.5A6.5 6.5 0 0 1 9.5 3m0 2C7 5 5 7 5 9.5S7 14 9.5 14S14 12 14 9.5S12 5 9.5 5Z"/>',
@@ -98,6 +101,19 @@ const globalStyles = globalCss({
     display: 'block',
     verticalAlign: 'middle',
     overflow: 'visible',
+  },
+
+  button: {
+    m: 0,
+    p: 0,
+    border: 0,
+    outline: 0,
+    background: 'none',
+    boxShadow: 'none',
+    color: 'inherit',
+    font: 'inherit',
+    textAlign: 'inherit',
+    textTransform: 'none',
   },
 
   '#__next': {
