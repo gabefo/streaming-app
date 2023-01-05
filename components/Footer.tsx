095c7716d5903cebe445b8a3c87c0b3f1c51e139
gabefo <gabrielfomiranda@gmail.com>
2023-01-04 23:05:45 -0300

    change styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,40 +4,26 @@ import { styled } from 'stitches.config'
 import Container from './Container'
 import Flex from './Flex'
 import LanguageSelect from './LanguageSelect'
-import Logo from './Logo'
 import SocialLink from './SocialLink'
 import Text from './Text'
 import ThemeSelect from './ThemeSelect'
 import TMDBLogo from './TMDBLogo'
 
-const StyledFooter = styled('footer', {
+const Root = styled('footer', {
   display: 'flex',
   alignItems: 'center',
   pb: 24,
 })
 
-const Separator = styled('div', {
+const Divider = styled('div', {
   my: 32,
   borderBottom: 'thin solid $border',
-})
-
-const Section = styled('section', {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'flex-start',
-  mt: 32,
-  gap: 24,
-
-  '@md': {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
 })
 
 const StyledLink = styled(Link, {
   color: '$textSecondary',
   fontSize: '0.875rem',
-  lineHeight: '2rem',
+  lineHeight: '1.25rem',
   textDecoration: 'none',
 
   '&:hover': {
@@ -47,37 +33,58 @@ const StyledLink = styled(Link, {
 
 export default function Footer() {
   return (
-    <StyledFooter>
+    <Root>
       <Container gutters>
-        <Separator />
-        <Section>
-          <Logo />
-          <Flex direction="column">
+        <Divider />
+        <Flex
+          direction={{ '@initial': 'column', '@md': 'row' }}
+          align="start"
+          justify="between"
+          css={{ gap: 24 }}
+        >
+          <Flex direction="column" css={{ maxWidth: 240, gap: 24 }}>
+            <TMDBLogo />
+            <Text variant="caption" color="disabled">
+              This product uses the TMDB API but is not endorsed or certified by TMDB.
+            </Text>
+            <Flex css={{ gap: 8 }}>
+              <SocialLink href="https://github.com/gabefo/streaming-app" target="_blank">
+                <Icon icon="mdi:github" />
+              </SocialLink>
+              <SocialLink href="https://www.facebook.com/" target="_blank">
+                <Icon icon="mdi:facebook" />
+              </SocialLink>
+              <SocialLink href="https://www.linkedin.com/in/gabrielfomiranda/" target="_blank">
+                <Icon icon="mdi:linkedin" />
+              </SocialLink>
+            </Flex>
+          </Flex>
+          <Flex direction="column" css={{ gap: 12 }}>
+            <Text variant="overline">Watch</Text>
             <StyledLink href="/">Home</StyledLink>
             <StyledLink href="/about">About</StyledLink>
+            <StyledLink href="/contact">Contact</StyledLink>
+          </Flex>
+          <Flex direction="column" css={{ gap: 12 }}>
+            <Text variant="overline">Legal</Text>
             <StyledLink href="/terms-of-service">Terms of Service</StyledLink>
             <StyledLink href="/privacy">Privacy</StyledLink>
-          </Flex>
-          <Flex css={{ gap: 16 }}>
-            <SocialLink href="https://github.com/gabefo/streaming-app" target="_blank">
-              <Icon icon="mdi:github" />
-            </SocialLink>
-            <SocialLink href="https://www.linkedin.com/in/gabrielfomiranda/" target="_blank">
-              <Icon icon="mdi:linkedin" />
-            </SocialLink>
           </Flex>
           <Flex css={{ gap: 8 }}>
             <LanguageSelect />
             <ThemeSelect />
           </Flex>
-        </Section>
-        <Section>
-          <Text variant="caption" color="disabled">
-            This product uses the TMDB API but is not endorsed or certified by TMDB.
-          </Text>
-          <TMDBLogo />
-        </Section>
+        </Flex>
+        <Text
+          as="p"
+          variant="caption"
+          color="disabled"
+          align={{ '@initial': 'center', '@md': 'left' }}
+          css={{ mt: 40 }}
+        >
+          © {new Date().getFullYear()}. All rights reserved
+        </Text>
       </Container>
-    </StyledFooter>
+    </Root>
   )
 }
